76860e46050d02245e68c3e54f2f9fcd05e3a4c0
cityflysky <cityflysky@gmail.com>
2022-09-24 11:26:45 +0800

    update join

--- contracts/cityzen.sol
+++ contracts/cityzen.sol
@@ -2,11 +2,11 @@
 pragma solidity 0.8.6;
 
 contract citizen {
-    
+
     mapping(int256 => address) public  citizenMap;
     mapping(address=>int64) cityTypeMap;
     int256 citizenNumber = 0;
-    function joinCity() public external{
+    function joinCity()  public{
         citizenMap[citizenNumber++] = msg.sender;
         cityTypeMap[msg.sender] = 1;
     }
